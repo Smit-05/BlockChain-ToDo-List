727558cd0b94b258a10882b1b62b16d72c711d3a
Smit Padaliya <smitpadaliya5@gmail.com>
2022-06-14 20:57:01 +0530

    .

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,6 +2,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList{
     uint public taskCount=0;
-    
-        
+
+    struct Task{
+        uint id;
+        string taskName;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor () public {
+        createTask("Start doig tasks now :)");
+    }
+
+    function createTask(string memory content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount,content,false);
+
+    }
 }
---
+++ src/app.js
@@ -0,0 +1,52 @@
+
+App = {
+  load: async () => {
+    await App.loadWeb3()
+    await App.loadAccount()
+  },
+
+  loadWeb3: async () => {
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider
+      web3 = new Web3(web3.currentProvider)
+    } else {
+      window.alert("Please connect to Metamask.")
+    }
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum)
+      try {
+        // Request account access if needed
+        await ethereum.enable()
+        // Acccounts now exposed
+        web3.eth.sendTransaction({/* ... */})
+      } catch (error) {
+        // User denied account access...
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = web3.currentProvider
+      window.web3 = new Web3(web3.currentProvider)
+      // Acccounts always exposed
+      web3.eth.sendTransaction({/* ... */})
+    }
+    // Non-dapp browsers...
+    else {
+      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  },
+
+  loadAccount: async () => {
+    App.account = window.web3.eth.accounts[0]
+    console.log(App.account)
+  }
+
+
+}
+
+$(() => {
+  $(window).load(() => {
+    App.load()
+  })
+})
